2cc402aa514074c91695577a48a110e126fb9c03
Ali Karabay <akarabay@k1m05s02.42istanbul.com.tr>
2022-04-24 21:46:37 +0300

    updated file.

--- push_swap/sort_the_list.c
+++ push_swap/sort_the_list.c
@@ -6,128 +6,128 @@
 /*   By: akarabay <akarabay@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/15 14:38:52 by akarabay          #+#    #+#             */
-/*   Updated: 2022/04/23 16:09:18 by akarabay         ###   ########.fr       */
+/*   Updated: 2022/04/24 18:23:38 by akarabay         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	make_pivot(t_data *s)
+int	make_pivot(t_data *str)
 {
-    int		i;
-    long	min;
-    long	max;
+	int		index;
+	long	min;
+	long	max;
 
-    i = 0;
-    min = INT_MAX;
-    max = INT_MIN;
-    while (i++ < s->size)
-    {
-        if (s->array[s->size - i] > max)
-            max = s->array[s->size - i];
-        if (s->array[s->size - i] < min)
-            min = s->array[s->size - i];
-    }
-    return ((max + min) / 2);
+	index = 0;
+	min = INT_MAX;
+	max = INT_MIN;
+	while (index++ < str->size)
+	{
+		if (str->array[str->size - index] > max)
+			max = str->array[str->size - index];
+		if (str->array[str->size - index] < min)
+			min = str->array[str->size - index];
+	}
+	return ((max + min) / 2);
 }
 
 //bulunan pivot değeri altındakileri pushlanacak
-void	send_b(t_data *a, t_data *b)
+void	send_b(t_data *data_a, t_data *data_b)
 {
-    int	pivot;
-    int	i;
-    int	total_numbers;
+	int	pivot;
+	int	index;
+	int	total_numbers;
 
-    i = 0;
-    total_numbers = a->size;
-    pivot = make_pivot(a);
-    if (a->size < 3)
-        return ;
-    while (total_numbers > i)
-    {
-        if (a->array[a->size - 1] <= pivot)
-            push(a, b);
-        else
-            rotate(a, 0);
-        i++;
-    }
-    send_b(a, b);
+	index = 0;
+	total_numbers = data_a->size;
+	pivot = make_pivot(data_a);
+	if (data_a->size < 3)
+		return ;
+	while (total_numbers > index)
+	{
+		if (data_a->array[data_a->size - 1] <= pivot)
+			push(data_a, data_b);
+		else
+			rotate(data_a, 0);
+		index++;
+	}
+	send_b(data_a, data_b);
 }
 
 //eğer b stağindeki değerin a daki yeri bulunmazsa en yüksek değer geri dönecek
-int	find_max_value(t_data *a)
+int	find_max_value(t_data *data_a)
 {
-    int	i;
-    int	max;
-    int	index;
+	int	two_index;
+	int	max;
+	int	index;
 
-    i = 0;
-    max = INT_MIN;
-    while (i < a->size)
-    {
-        if (a->array[i] > max)
-        {
-            max = a->array[i];
-            index = i;
-        }
-        i++;
-    }
-    return (index);
+    two_index = 0;
+	max = INT_MIN;
+	while (two_index < data_a->size)
+	{
+		if (data_a->array[two_index] > max)
+		{
+			max = data_a->array[two_index];
+			index = two_index;
+		}
+		two_index++;
+	}
+	return (index);
 }
 
 //b stağindeki değerin a daki en uygun index numarası
-int	find_index(t_data *a, int numb)
+int	find_index(t_data *data_a, int number)
 {
-    int	i;
-    int	min;
-    int	diff;
-    int	index;
+	int	two_index;
+	int	min;
+	int	diff;
+	int	index;
 
-    i = 0;
-    min = INT_MAX;
-    index = -1;
-    while (i < a->size)
-    {
-        diff = a->array[a->size - 1 - i] - numb;
-        if (diff < min && diff > 0)
-        {
-            min = diff;
-            index = i;
-        }
-        i++;
-    }
-    if (index == -1)
-        index = find_max_value(a);
-    return (index);
+	two_index = 0;
+	min = INT_MAX;
+	index = -1;
+	while (two_index < data_a->size)
+	{
+		diff = data_a->array[data_a->size - 1 - two_index] - number;
+		if (diff < min && diff > 0)
+		{
+			min = diff;
+			index = two_index;
+		}
+		two_index++;
+	}
+	if (index == -1)
+		index = find_max_value(data_a);
+	return (index);
 }
 
 /*en kısa yolu bulan fonksiyon, b deki her değer için a daki
 * yollar hesaplanır takibinde en kısası işlem görür*/
-void	find_sort_path(t_data *a, t_data *b, int *i_a, int *i_b)
+void	find_sort_path(t_data *data_a, t_data *data_b, int *index_a, int *index_b)
 {
-    int	tmp[2];
-    int	m;
-    int	n;
-    int	min;
+	int	tmp[2];
+	int	m;
+	int	n;
+	int	min;
 
-    min = INT_MAX;
-    m = -1;
-    while (++m < b->size)
-    {
-        n = find_index(a, b->array[b->size - m - 1]);
-        if (m < b->size / 2)
-            tmp[0] = m;
-        else
-            tmp[0] = b->size - m - 1;
-        if (n < a->size / 2)
-            tmp[1] = n;
-        else
-            tmp[1] = a->size - n - 1;
-        if (tmp[0] + tmp[1] < min)
-        {
-            min = tmp[0] + tmp[1];
-            *i_b = m;
-            *i_a = n;
-        }
-    }
+	min = INT_MAX;
+	m = -1;
+	while (++m < data_b->size)
+	{
+		n = find_index(data_a, data_b->array[data_b->size - m - 1]);
+		if (m < data_b->size / 2)
+			tmp[0] = m;
+		else
+			tmp[0] = data_b->size - m - 1;
+		if (n < data_a->size / 2)
+			tmp[1] = n;
+		else
+			tmp[1] = data_a->size - n - 1;
+		if (tmp[0] + tmp[1] < min)
+		{
+			min = tmp[0] + tmp[1];
+			*index_b = m;
+			*index_a = n;
+		}
+	}
 }
